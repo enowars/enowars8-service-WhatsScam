4ba7bdf5d774682d2ebbf26c0e2d4c89d162879d
SirGankalot <73303677+SirGankalot@users.noreply.github.com>
2024-05-01 19:00:05 +0200

    Visibility of Groups

--- service/src/FlaskWebsite/website/models.py
+++ service/src/FlaskWebsite/website/models.py
@@ -2,28 +2,6 @@ from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
 
-# NoteGroup = db.Table('NoteGroup',
-#     db.Column('id', db.Integer, primary_key=True),
-#     db.Column('name', db.String(150)),
-#     db.Column('note_id', db.Integer, db.ForeignKey('Note.id')),
-#     db.Column('user_id', db.Integer, db.ForeignKey('User.id'))
-# )
-
-# class Note(db.Model):
-#     __tablename__ = 'Note'
-#     id = db.Column(db.Integer, primary_key=True)
-#     data = db.Column(db.String(10000))
-#     date = db.Column(db.DateTime(timezone=True), default=func.now())
-#     user_id = db.Column(db.Integer, db.ForeignKey('User.id'))  # Foreign key relationship to User
-
-# class User(db.Model, UserMixin):
-#     __tablename__ = 'User'
-#     id = db.Column(db.Integer, primary_key=True)
-#     email = db.Column(db.String(150), unique=True)
-#     password = db.Column(db.String(150))
-#     first_name = db.Column(db.String(150))
-#     notes = db.relationship('Note', secondary=NoteGroup, backref='users')
-
 NoteGroup = db.Table('NoteGroup',
     db.Column('id', db.Integer, primary_key=True),
     db.Column('name', db.String(150)),
