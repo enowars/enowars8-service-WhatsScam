fb4aad8043d7635c1b7ba50d53b66d92a447792d
SirGankalot <73303677+SirGankalot@users.noreply.github.com>
2024-05-24 15:39:11 +0200

    testing get and putnoise

--- checker/src/checker.py
+++ checker/src/checker.py
@@ -290,7 +290,89 @@ async def exploit_test(
             except:
                 pass
     raise MumbleException("flag not found")
+
+@checker.putnoise(0)
+async def putnoise0(
+    task: PutnoiseCheckerTaskMessage,
+    db: ChainDB,
+    logger: LoggerAdapter,
+    conn: AsyncSocket,
+) -> None:
+    try:
+        client = AsyncClient(
+                base_url=f"http://{task.address}:{SERVICE_PORT}",
+                #timeout=timeout
+            )
+    except:
+        raise MumbleException("Could not connect to service")
+        
+    try:
+        email_1, password1_1 = await checker_util_func.create_user(db, client, logger, public_key='on')
+    except:
+        raise MumbleException("Could not create user 1")
+
+    try:
+        await checker_util_func.logout(db, client, logger)
+    except:
+        raise MumbleException("Could not logout")
+
+    try:
+        email_2, password1_2 = await checker_util_func.create_user(db, client, logger, public_key= None)
+    except:
+        raise MumbleException("Could not create user 2")
     
+    try:
+        public_key = await checker_util_func.get_user_of_userlist(db, client, logger, email = email_1)
+    except:
+        raise MumbleException("Could not get public key")
+
+    random.seed(random.SystemRandom().random())
+    randomNumber = random.randint(10, 1000)
+    randomNote = "".join(random.choices(string.ascii_letters + string.digits, k=randomNumber))
+    try:
+        await checker_util_func.create_note(db ,client, logger, randomNote, public_key)
+    except:
+        raise MumbleException("Could not create note")
+    
+    try:
+        await db.set("userdata", (email_2, password1_2, randomNote))
+    except:
+        raise MumbleException("Could not set userdata")
+    
+@checker.getnoise(0)
+async def getnoise0(
+    task: GetnoiseCheckerTaskMessage,
+    db: ChainDB,
+    logger: LoggerAdapter,
+    conn: AsyncSocket,
+) -> None:
+    try:
+        client = AsyncClient(
+                base_url=f"http://{task.address}:{SERVICE_PORT}",
+                #timeout=timeout
+            )
+    except:
+        raise MumbleException("Could not connect to service")
+    try:
+        email, password, note = await db.get("userdata")
+    except KeyError:
+        raise MumbleException("Missing database entry from putnoise")
+
+    try:
+        await checker_util_func.login_user(db, client, logger, email, password)
+    except:
+        raise MumbleException("Could not login user")
+    
+    try:
+        await checker_util_func.get_note(db, client, logger, note = note)
+    except:
+        raise MumbleException("Could not get note")
+    
+    
+
+
+
+
 
 """
 CHECKER FUNCTIONS 1
@@ -410,92 +492,6 @@ async def exploit_test_1(
     print("flag hier")
     return flag
 
-
-
-
-
-
-
-
-
-
-
-    
-
-    
-
-
-
-
-
-
-
-
-    # # Log a message before any critical action that could raise an error.
-    # logger.debug(f"Connecting to service")
-    # welcome = await conn.reader.readuntil(b">")
-
-    # # Register a new user
-    # await conn.register_user(username, password)
-
-    # # Now we need to login
-    # await conn.login_user(username, password)
-
-    # # Finally, we can post our note!
-    # logger.debug(f"Sending command to set the flag")
-    # conn.writer.write(f"set {task.flag}\n".encode())
-    # await conn.writer.drain()
-    # await conn.reader.readuntil(b"Note saved! ID is ")
-
-    # try:
-    #     # Try to retrieve the resulting noteId. Using rstrip() is hacky, you should probably want to use regular expressions or something more robust.
-    #     noteId = (await conn.reader.readuntil(b"!\n>")).rstrip(b"!\n>").decode()
-    # except Exception as ex:
-    #     logger.debug(f"Failed to retrieve note: {ex}")
-    #     raise MumbleException("Could not retrieve NoteId")
-
-    # assert_equals(len(noteId) > 0, True, message="Empty noteId received")
-
-    # logger.debug(f"Got noteId {noteId}")
-
-    # # Exit!
-    # logger.debug(f"Sending exit command")
-    # conn.writer.write(f"exit\n".encode())
-    # await conn.writer.drain()
-    
-    # Save the generated values for the associated getflag() call.
-    #await db.set("userdata", (username, password, noteId))
-
-    #return username
-
-# @checker.getflag(0)
-# async def getflag_note(
-#     task: GetflagCheckerTaskMessage, db: ChainDB, logger: LoggerAdapter, conn: Connection
-# ) -> None:
-#     try:
-#         username, password, noteId = await db.get("userdata")
-#     except KeyError:
-#         raise MumbleException("Missing database entry from putflag")
-
-#     logger.debug(f"Connecting to the service")
-#     await conn.reader.readuntil(b">")
-
-#     # Let's login to the service
-#     await conn.login_user(username, password)
-
-#     # Let´s obtain our note.
-#     logger.debug(f"Sending command to retrieve note: {noteId}")
-#     conn.writer.write(f"get {noteId}\n".encode())
-#     await conn.writer.drain()
-#     note = await conn.reader.readuntil(b">")
-#     assert_in(
-#         task.flag.encode(), note, "Resulting flag was found to be incorrect"
-#     )
-
-#     # Exit!
-#     logger.debug(f"Sending exit command")
-#     conn.writer.write(f"exit\n".encode())
-#     await conn.writer.drain()
         
 
 # @checker.putnoise(0)
@@ -667,63 +663,9 @@ async def exploit_test_1(
 #     if not noteId.encode() in data:
 #         raise MumbleException("List command does not work as intended")
 
-# @checker.exploit(0)
-# async def exploit0(task: ExploitCheckerTaskMessage, searcher: FlagSearcher, conn: Connection, logger:LoggerAdapter) -> Optional[str]:
-#     welcome = await conn.reader.readuntil(b">")
-#     conn.writer.write(b"dump\nexit\n")
-#     await conn.writer.drain()
-#     data = await conn.reader.read(-1)
-#     if flag := searcher.search_flag(data):
-#         return flag
-#     raise MumbleException("flag not found")
 
-# @checker.exploit(1)
-# async def exploit1(task: ExploitCheckerTaskMessage, searcher: FlagSearcher, conn: Connection, logger:LoggerAdapter) -> Optional[str]:
-#     welcome = await conn.reader.readuntil(b">")
-#     conn.writer.write(b"user\n")
-#     await conn.writer.drain()
 
-#     # TODO: Use flag hints
-#     user_list = (await conn.reader.readuntil(b">")).split(b"\n")[:-1]
-#     for user in user_list:
-#         user_name = user.split()[-1]
-#         conn.writer.write(b"reg %s foo\nlog %s foo\n list\n" % (user_name, user_name))
-#         await conn.writer.drain()
-#         await conn.reader.readuntil(b">")  # successfully registered
-#         await conn.reader.readuntil(b">")  # successfully logged in
-#         notes_list = (await conn.reader.readuntil(b">")).split(b"\n")[:-1]
-#         for note in notes_list:
-#             note_id = note.split()[-1]
-#             conn.writer.write(b"get %s\n" % note_id)
-#             await conn.writer.drain()
-#             data = await conn.reader.readuntil(b">")
-#             if flag := searcher.search_flag(data):
-#                 return flag
-#     raise MumbleException("flag not found")
 
-# @checker.exploit(2)
-# async def exploit2(task: ExploitCheckerTaskMessage, searcher: FlagSearcher, conn: Connection, logger:LoggerAdapter) -> Optional[str]:
-#     welcome = await conn.reader.readuntil(b">")
-#     conn.writer.write(b"user\n")
-#     await conn.writer.drain()
-
-#     # TODO: Use flag hints?
-#     user_list = (await conn.reader.readuntil(b">")).split(b"\n")[:-1]
-#     for user in user_list:
-#         user_name = user.split()[-1]
-#         conn.writer.write(b"reg ../users/%s foo\nlog %s foo\n list\n" % (user_name, user_name))
-#         await conn.writer.drain()
-#         await conn.reader.readuntil(b">")  # successfully registered
-#         await conn.reader.readuntil(b">")  # successfully logged in
-#         notes_list = (await conn.reader.readuntil(b">")).split(b"\n")[:-1]
-#         for note in notes_list:
-#             note_id = note.split()[-1]
-#             conn.writer.write(b"get %s\n" % note_id)
-#             await conn.writer.drain()
-#             data = await conn.reader.readuntil(b">")
-#             if flag := searcher.search_flag(data):
-#                 return flag
-#     raise MumbleException("flag not found")
 
 
 if __name__ == "__main__":
