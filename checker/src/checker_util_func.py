5a9c5839b5477098f116f33da756cd6ee0a7d711
SirGankalot <73303677+SirGankalot@users.noreply.github.com>
2024-06-05 11:38:07 +0200

    try it with 2.0 seconds timeout

--- checker/src/checker_util_func.py
+++ checker/src/checker_util_func.py
@@ -87,7 +87,7 @@ async def create_user(
             "password2": password2,
         },
         follow_redirects=True,
-        #timeout=1.0, #standard timeout 5.0
+        timeout=2.0, #standard timeout 5.0
     )
 
     logger.info(f"Server answered: {response.status_code} - {response.text}")
