29fa9cec4f730b58b94d14ee6b147b2d3cbd13f5
SirGankalot <73303677+SirGankalot@users.noreply.github.com>
2024-06-13 15:51:42 +0200

    changing ports in docker so it doesnt forward but insta claims 19696

--- checker/src/gunicorn.conf.py
+++ checker/src/gunicorn.conf.py
@@ -2,7 +2,7 @@ import multiprocessing
 
 worker_class = "uvicorn.workers.UvicornWorker"
 workers = min(4, multiprocessing.cpu_count())
-bind = "0.0.0.0:8000"
+bind = "0.0.0.0:19696"
 timeout = 90
 keepalive = 3600
 preload_app = True
