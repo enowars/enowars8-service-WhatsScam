e892856661afb9bdc61869db94f252c698dd18ff
SirGankalot <73303677+SirGankalot@users.noreply.github.com>
2024-06-18 00:02:11 +0200

    changing workers do dynamic

--- service/gunicorn.conf.py
+++ service/gunicorn.conf.py
@@ -1,7 +1,7 @@
 import multiprocessing
 
 #worker_class = "gevent"
-workers = 3 #min(4, multiprocessing.cpu_count())
+workers = min(4, multiprocessing.cpu_count())
 bind = "0.0.0.0:9696"
 timeout = 90
 keepalive = 3600
